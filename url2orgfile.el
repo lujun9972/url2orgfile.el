0c0c1a10f0f069d66b79c603c1bf59a4bd34cdab
darksun <lujun9972@gmail.com>
2017-04-13 18:17:57 +0800

    update

--- url2orgfile.el
+++ url2orgfile.el
@@ -1,4 +1,39 @@
-;; -*- lexical-binding: t; -*-
+;;; url2orgfile.el --- Convert html to org format text   -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2017-2018 DarkSun
+
+;; Author: DarkSun <lujun9972@gmail.com>
+;; Created: 2017-4-10
+;; Version: 0.1
+;; Keywords: convenience, html, org
+;; Package-Requires: ((emacs "24.4"))
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Source code
+;;
+;; url2orgfile's code can be found here:
+;;   http://github.com/lujun9972/url2orgfile.el
+
+
+;;; Commentary:
+;; 
+
+;;; Code:
+
 (require 'dom)
 (require 'html2org)
 (defgroup url2orgfile nil
@@ -62,3 +97,5 @@ This function will return the saved ORG-FILE path"
                             (message "%s saved to %s" url org-file)))))))
 
 (provide 'url2orgfile)
+
+;;; url2orgfile.el ends here
